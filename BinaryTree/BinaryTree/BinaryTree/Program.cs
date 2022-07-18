2b0e6936c4fabc30ff25a551af6d5d4b15250bcc
KIBA <kjh030529@gmail.com>
2022-07-19 08:18:52 +0900

    Maybe Complete C# BinaryTree

--- BinaryTree/BinaryTree/BinaryTree/Program.cs
+++ BinaryTree/BinaryTree/BinaryTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BinaryTree
 {
@@ -14,9 +15,9 @@ namespace BinaryTree
 
     public class CustomTree<T> where T : class
     {
-        enum direction { Left, Right }
+        public enum direction { Left, Right }
 
-        class node
+        public class node
         {
             public T data;
             public node left;
@@ -28,12 +29,19 @@ namespace BinaryTree
                 this.left = left;
                 this.right = right;
             }
+
+            public override string ToString()
+            {
+                return $"(Node|{data})";
+            }
         }       
         
-        public CustomTree()
-        {  }
+        public CustomTree(node Root)
+        { this.Root = Root; }
 
-        public void Add(node parent, direction dir, T data)
+        public node Root;
+
+        public node Add(node parent, direction dir, T data)
         {
             switch(dir)
             {
@@ -41,14 +49,81 @@ namespace BinaryTree
                     var newLeftNode = new node(data, null, null);
                     if (parent.left == null) parent.left = newLeftNode;
                     else Console.WriteLine("[BinaryTree] Left is already exist");
-                    break;
+                    return newLeftNode;
 
                 case direction.Right:
                     var newRightNode = new node(data, null, null);
-                    if (parent.left == null) parent.right = newRightNode;
+                    if (parent.right == null) parent.right = newRightNode;
                     else Console.WriteLine("[BinaryTree] Right is already exist");
+                    return newRightNode;
+            }
+
+            return null;
+        }
+
+        public void Remove(node parent, direction dir, T data)
+        {
+            switch (dir)
+            {
+                case direction.Left:
+                    parent.left = null;
+                    break;
+
+                case direction.Right:
+                    parent.right = null;
                     break;
             }
+        }
+
+        public List<node> GetNodesInOrder()
+        {
+            var treeNodes = new List<node>();
+
+            _InOrder(Root, treeNodes);
+
+            return treeNodes;
+        }
+
+        public List<node> GetNodesPreOrder()
+        {
+            var treeNodes = new List<node>();
+
+            _PreOrder(Root, treeNodes);
+
+            return treeNodes;
+        }
+
+        public List<node> GetNodesPostOrder()
+        {
+            var treeNodes = new List<node>();
+
+            _PostOrder(Root, treeNodes);
+
+            return treeNodes;
+        }
+
+        private void _InOrder(node? parent, List<node> list)
+        {
+            if (parent == null) return;
+            _InOrder(parent.left, list);
+            list.Add(parent);
+            _InOrder(parent.right, list);
+        }
+
+        private void _PreOrder(node? parent, List<node> list)
+        {
+            if (parent == null) return;
+            list.Add(parent);
+            _InOrder(parent.left, list);
+            _InOrder(parent.right, list);
+        }
+
+        private void _PostOrder(node? parent, List<node> list)
+        {
+            if (parent == null) return;
+            _InOrder(parent.left, list);
+            _InOrder(parent.right, list);
+            list.Add(parent);
         }
     }
 }
