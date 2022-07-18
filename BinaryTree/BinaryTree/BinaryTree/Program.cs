e6c90a73a7d3e581d84fdbbc426c18ebb0d8ed98
KIBA <kjh030529@gmail.com>
2022-07-18 17:56:16 +0900

    Binary Tree (WIP)

---
+++ BinaryTree/BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+
+    public class CustomTree<T> where T : class
+    {
+        enum direction { Left, Right }
+
+        class node
+        {
+            public T data;
+            public node left;
+            public node right;
+
+            public node(T data, node left, node right)
+            {
+                this.data = data;
+                this.left = left;
+                this.right = right;
+            }
+        }       
+        
+        public CustomTree()
+        {  }
+
+        public void Add(node parent, direction dir, T data)
+        {
+            switch(dir)
+            {
+                case direction.Left:
+                    var newLeftNode = new node(data, null, null);
+                    if (parent.left == null) parent.left = newLeftNode;
+                    else Console.WriteLine("[BinaryTree] Left is already exist");
+                    break;
+
+                case direction.Right:
+                    var newRightNode = new node(data, null, null);
+                    if (parent.left == null) parent.right = newRightNode;
+                    else Console.WriteLine("[BinaryTree] Right is already exist");
+                    break;
+            }
+        }
+    }
+}
